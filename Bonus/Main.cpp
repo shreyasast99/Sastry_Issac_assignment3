dee60178b38bd700e085846405a8ab653a7f6226
Shreya Sastry <srs93414@uga.edu>
2020-03-30 17:30:59 -0400

    only did int version

--- Bonus/Main.cpp
+++ Bonus/Main.cpp
@@ -1,7 +1,7 @@
 #include<fstream>
 
 #include<iostream>
-
+#include<string>
 #include "DoublyLinkedList.h"
 
 using namespace std;
@@ -23,7 +23,9 @@ int main(int argc, char *argv[]){
   string inputString;
   float inputFloat;
 
-  int number=0;//nums to add
+  int numberi=0;//nums to add
+  float numberf=0;
+  string str="";
   /*
   std::fstream fs;
 
@@ -57,33 +59,82 @@ int main(int argc, char *argv[]){
 */
 //    list.print();
 
-
   cout<< "\nEnter list type (i- int, f - float, s- std:string):";
   cin>>userInput;
   
   //stores int values
   if(userInput.compare("i")){
-    std::fstream fs
-      fs.open(argv[1], std::fstream::in);
-    if (fs.is_open()){
-      fs >> inputInt;
-      while (!fs.eof()){
-	listInt.insertItem(itemInt);
-	fs >> inputInt;
+      std::fstream fs;
+          fs.open(argv[1], std::fstream::in);
+      if (fs.is_open()){
+          fs >> inputInt;
+          while (!fs.eof()){
+              listInt.insertItem(itemInt);
+              fs >> inputInt;
+          }
       }
-    }
-    else{
-      std::cout << "Failed to open the input file" << std::endl;
-      return 0;
-    }
-  }
-  
-  cout<<"insert (i), delete (d), length(l), print (p), quit (q)"<<endl;
-    while(userInput.compare("q")!=0){
+      else{
+          std::cout << "Failed to open the input file" << std::endl;
+          return 0;
+      }
+      cout<<"insert (i), delete (d), length(l), print (p), quit (q)"<<endl;
+      while(userInput.compare("q")!=0){
         cout<<"\nEnter a command: ";
         cin>>userInput;
         if((userInput.compare("p")!=0)&&(userInput.compare("r")!=0)&&(userInput.compare("s")!=0)&&(userInput.compare("n")!=0)){
+            
+            //          cout<<"Hi"<<endl;
 
+            listInt.print(); //prints the list
+            cout<<endl;
+        }
+
+        //ItemType temp;
+        //insert value
+        if(userInput.compare("i")==0){
+            cout<<"Number to insert: ";
+            cin>>numberi;
+            //temp.initialize(number);
+            
+            listInt.insertItem(numberi);
+            listInt.print();
+            cout<<endl;
+        }
+        else if(userInput.compare("d")==0){
+            cout<<"Number to delete: ";
+            cin>>numberi;
+            //temp.initialize(number);
+            listInt.deleteItem(numberi);
+            listInt.print();
+        }
+        else if(userInput.compare("p")==0){
+            listInt.print();
+        }
+        else if(userInput.compare("l")==0){
+            cout<<"List Length is "<<listInt.lengthIs();
+        }
+        else if(userInput.compare("q")==0){
+            cout<<"Quitting program...";
+        }
+        else if(userInput.compare("r")==0){
+            listInt.printReverse();
+//            list.print();
+        }
+        //invalid command
+        else{
+            cout<<"Invalid command, try again!";
+        }
+        cout<<endl;
+    }
+      
+  }
+  /*
+  cout<<"insert (i), delete (d), length(l), print (p), quit (q)"<<endl;
+  while(userInput.compare("q")!=0){
+        cout<<"\nEnter a command: ";
+        cin>>userInput;
+        if((userInput.compare("p")!=0)&&(userInput.compare("r")!=0)&&(userInput.compare("s")!=0)&&(userInput.compare("n")!=0)){
+            
             //          cout<<"Hi"<<endl;
 
             list.print(); //prints the list
@@ -96,71 +147,40 @@ int main(int argc, char *argv[]){
             cout<<"Number to insert: ";
             cin>>number;
             temp.initialize(number);
-
             
-
             list.insertItem(temp);
-
             list.print();
-
             cout<<endl;
-
         }
-
         else if(userInput.compare("d")==0){
-
             cout<<"Number to delete: ";
-
             cin>>number;
-
             temp.initialize(number);
-
             list.deleteItem(temp);
-
             list.print();
-
         }
-
         else if(userInput.compare("p")==0){
-
             list.print();
-
         }
-
         else if(userInput.compare("l")==0){
-
             cout<<"List Length is "<<list.lengthIs();
-
         }
-
         else if(userInput.compare("q")==0){
-
             cout<<"Quitting program...";
-
         }
-
         else if(userInput.compare("r")==0){
-
             list.printReverse();
-
 //            list.print();
-
         }
-
         //invalid command
-
         else{
-
             cout<<"Invalid command, try again!";
-
         }
-
         cout<<endl;
-
     }
-
+  */
 }
-
+  
 /*
 
 using namespace std;
